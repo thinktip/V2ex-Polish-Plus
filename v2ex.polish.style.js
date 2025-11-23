a90375512f90f8a785310f2558b31b3172f4bd54
Thinktip <thinktip@gmail.com>
2025-11-23 14:42:49 +0800

    Update v2ex.polish.style.js

--- v2ex.polish.style.js
+++ v2ex.polish.style.js
@@ -2,6 +2,9 @@
 // @name         V2eX - Polish Style
 // @version      3.1
 // @description  V2ex Polish Style
+// @author       coolpace
+// @author       2smile
+// @author       ChatGPT
 // @match        https://*.v2ex.com/*
 // @match        https://v2ex.com/*
 // @icon         https://v2ex.com/static/apple-touch-icon-180.png
